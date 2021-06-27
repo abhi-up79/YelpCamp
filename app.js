a0143e4a07dbda4189d727c893879c48962d34a9
Abhishek Sharma <sharma.abhishek208@gmail.com>
2021-06-27 21:08:36 +0530

    Added image upload, maps, cluster maps, styles clean up

--- app.js
+++ app.js
@@ -1,56 +1,61 @@
-const express = require('express');
-const session = require('express-session');
-const flash = require('connect-flash')
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const ExpressError = require('./utils/ExpressError');
-const methodOverride = require('method-override')
-const passport = require('passport');
-const LocalStrategy = require('passport-local')
-const User = require('./models/user');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
+const express = require("express");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-const campgroundRoutes = require('./routes/campgrounds');
-const reviewRoutes = require('./routes/reviews');
-const userRoutes = require('./routes/users'); 
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const mongoSanitize = require("express-mongo-sanitize");
 
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database Connected");
+  console.log("Database Connected");
 });
 
 const app = express();
 
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
-    secret: 'thissholdbeasecret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expire: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+  secret: "thissholdbeasecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,
+    expire: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
 
 app.use(passport.initialize());
@@ -61,34 +66,31 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    //console.log(req.session)
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
+  console.log(req.query);
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-app.use('/', userRoutes);
-app.use('/campgrounds', campgroundRoutes);
-app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page not Found', 404))
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh no, Something went wrong!';
-    res.status(statusCode).render('error', { err });
-})
-
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh no, Something went wrong!";
+  res.status(statusCode).render("error", { err });
+});
 
 app.listen(3000, () => {
-    console.log("Serving on port 3000!");
-})
+  console.log("Serving on port 3000!");
+});
--- seeds/index.js
+++ seeds/index.js
@@ -1,41 +1,60 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database Connected");
+  console.log("Database Connected");
 });
-
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            author: '60d46dd566764e45d8a794e1',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur eos veniam dolorem quibusdam velit, culpa repudiandae ad odit quas ex eveniet deleniti delectus deserunt laboriosam quis? Ratione beatae earum magni.',
-            price
-        })
-        await camp.save();
-    }
-}
+  await Campground.deleteMany({});
+  for (let i = 0; i < 300; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "60d46dd566764e45d8a794e1",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur eos veniam dolorem quibusdam velit, culpa repudiandae ad odit quas ex eveniet deleniti delectus deserunt laboriosam quis? Ratione beatae earum magni.",
+      price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+      images: [
+        {
+          url: "https://res.cloudinary.com/abhi-up79/image/upload/v1624767622/YelpCamp/ubkpaluzymhnv9l74ymw.jpg",
+          filename: "YelpCamp/ubkpaluzymhnv9l74ymw",
+        },
+        {
+          url: "https://res.cloudinary.com/abhi-up79/image/upload/v1624767624/YelpCamp/d7sklm0paogtamlasr2w.jpg",
+          filename: "YelpCamp/d7sklm0paogtamlasr2w",
+        },
+        {
+          url: "https://res.cloudinary.com/abhi-up79/image/upload/v1624767622/YelpCamp/bddwqqflvqmpsdoxlbe3.jpg",
+          filename: "YelpCamp/bddwqqflvqmpsdoxlbe3",
+        },
+      ],
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
-})
+  mongoose.connection.close();
+});
